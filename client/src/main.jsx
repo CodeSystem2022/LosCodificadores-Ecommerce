a101a8d8276bb75adcd93cc4aa735901467b104d
Agustin Vera <ronedev25@gmail.com>
2023-11-06 23:45:00 -0300

    feat: se actualizó el main de la aplicación utilizando react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./globals.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Router.jsx";
+import { ProductProvider } from "./context/ProductContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ProductProvider>
+      <RouterProvider router={router} />
+    </ProductProvider>
+  </React.StrictMode>
+);

6d4523fa2c10287212ae390b7f49c5b52c525488
Agustin Vera <ronedev25@gmail.com>
2023-11-10 08:44:15 -0300

    add: product routes in server

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,26 @@ const express = require('express')
 const productController = require('../controllers/ProductController.js')
 
 const router = express.Router()
+//Obtener todos los productos
+router.get('/', (req, res) => res.json("API CODIFICADORES"))
 
+//Obtener todos los productos
 router.get('/products', productController.getAllProducts)
+
+//Obtener un solo producto especifico
+router.get('/product/:id', productController.getOneProduct)
+
+//Obtener productos de categoria
+router.get('/products/:categoria', productController.getProductsByCategory)
+
+//Obtener los productos en oferta
+router.get('/get-offered', productController.getOfferedProducts)
+
+//Obtener los productos en oferta
+router.get('/product/get-random', productController.getRandomProduct)
+
+//Buscar producto
+router.get('/search/product', productController.searchProduct)
+
 
 module.exports = router

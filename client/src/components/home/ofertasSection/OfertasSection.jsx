60d63873a3471e0e9019c2368c6ef1054c8f73f4
Agustin Vera <ronedev25@gmail.com>
2023-11-10 04:06:35 -0300

    feat: update Carrousel y OfertasSection

--- client/src/components/home/ofertasSection/OfertasSection.jsx
+++ client/src/components/home/ofertasSection/OfertasSection.jsx
@@ -1,54 +1,16 @@
+import { useContext, useEffect, useState } from "react";
 import { Card } from "../../commons/Card/Card";
-import "./OfertasSection.css";
-
-const OFFERED_PRODUCTS = [
-  {
-    id: 0,
-    title: "Noblex - Intel i3",
-    description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-    images: [
-      "/src/assets/images/products/0/1.jpg",
-      "/src/assets/images/products/0/2.jpg",
-      "/src/assets/images/products/0/3.jpg",
-      "/src/assets/images/products/0/4.jpg",
-    ],
-  },
-  {
-    id: 1,
-    title: "Noblex - Intel i3",
-    description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-    images: [
-      "/src/assets/images/products/1/1.jpg",
-      "/src/assets/images/products/1/2.jpg",
-      "/src/assets/images/products/1/3.jpg",
-      "/src/assets/images/products/1/4.jpg",
-    ],
-  },
-  {
-    id: 2,
-    title: "Noblex - Intel i3",
-    description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-    images: [
-      "/src/assets/images/products/2/1.jpg",
-      "/src/assets/images/products/2/2.jpg",
-      "/src/assets/images/products/2/3.jpg",
-      "/src/assets/images/products/2/4.jpg",
-    ],
-  },
-  {
-    id: 3,
-    title: "Noblex - Intel i3",
-    description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-    images: [
-      "/src/assets/images/products/3/1.jpg",
-      "/src/assets/images/products/3/2.jpg",
-      "/src/assets/images/products/3/3.jpg",
-      "/src/assets/images/products/3/4.jpg",
-    ],
-  },
-];
+import { ProductContext } from "../../../context/ProductContext";
 
 const OfertasSection = () => {
+  const [products, setProducts] = useState([]);
+
+  const { getOfferedProducts } = useContext(ProductContext);
+
+  useEffect(() => {
+    getOfferedProducts().then((res) => setProducts(res.products));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <section id="ofertasSection">
       <div className="section-text">
@@ -56,11 +18,8 @@ const OfertasSection = () => {
         <span>En todos nuestras computadoras y artefactos</span>
       </div>
       <div className="itemsContainer">
-        {OFFERED_PRODUCTS.map((product) => (
-          <Card
-            key={product.id}
-            item={product}
-          />
+        {products.map((product) => (
+          <Card key={product.id} item={product} />
         ))}
       </div>
     </section>
@@ -68,3 +27,4 @@ const OfertasSection = () => {
 };
 
 export default OfertasSection;
+

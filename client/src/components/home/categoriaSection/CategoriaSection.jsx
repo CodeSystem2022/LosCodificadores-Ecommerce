1c5af7f1f160bf38678da2737ea0ac9b3d659155
Agustin Vera <ronedev25@gmail.com>
2023-11-10 04:11:22 -0300

    feat: update categoria section

--- client/src/components/home/categoriaSection/CategoriaSection.jsx
+++ client/src/components/home/categoriaSection/CategoriaSection.jsx
@@ -1,84 +1,33 @@
-import Carrusel from '../../commons/Carrusel/Carrusel'
-import './CategoriaSection.css'
+import { useContext, useEffect, useState } from "react";
+import Carrusel from "../../commons/Carrusel/Carrusel";
+import "./CategoriaSection.css";
+import { ProductContext } from "../../../context/ProductContext";
+import Loader from "../../commons/Loader/Loader";
 
-const CategoriaSection = ({categoriaTitle, }) => {
-    const CATEGORY_PRODUCTS = [
-        {
-          id: 0,
-          title: "Noblex - Intel i3",
-          description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-          images: [
-            "/src/assets/images/products/0/1.jpg",
-            "/src/assets/images/products/0/2.jpg",
-            "/src/assets/images/products/0/3.jpg",
-            "/src/assets/images/products/0/4.jpg",
-          ],
-        },
-        {
-          id: 1,
-          title: "Noblex - Intel i3",
-          description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-          images: [
-            "/src/assets/images/products/1/1.jpg",
-            "/src/assets/images/products/1/2.jpg",
-            "/src/assets/images/products/1/3.jpg",
-            "/src/assets/images/products/1/4.jpg",
-          ],
-        },
-        {
-          id: 2,
-          title: "Noblex - Intel i3",
-          description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-          images: [
-            "/src/assets/images/products/2/1.jpg",
-            "/src/assets/images/products/2/2.jpg",
-            "/src/assets/images/products/2/3.jpg",
-            "/src/assets/images/products/2/4.jpg",
-          ],
-        },
-        {
-          id: 3,
-          title: "Noblex - Intel i3",
-          description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-          images: [
-            "/src/assets/images/products/3/1.jpg",
-            "/src/assets/images/products/3/2.jpg",
-            "/src/assets/images/products/3/3.jpg",
-            "/src/assets/images/products/3/4.jpg",
-          ],
-        },
-        {
-          id: 4,
-          title: "Noblex - Intel i3",
-          description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-          images: [
-            "/src/assets/images/products/4/1.jpg",
-            "/src/assets/images/products/4/2.jpg",
-            "/src/assets/images/products/4/3.jpg",
-            "/src/assets/images/products/4/4.jpg",
-          ],
-        },
-        {
-          id: 5,
-          title: "Noblex - Intel i3",
-          description: "Intel i3, SSD 256GB, DDR4 8GB, BLA BLA BLA",
-          images: [
-            "/src/assets/images/products/5/1.jpg",
-            "/src/assets/images/products/5/2.jpg",
-            "/src/assets/images/products/5/3.jpg",
-            "/src/assets/images/products/5/4.jpg",
-          ],
-        },
-      ];
+const CategoriaSection = ({ categoriaTitle }) => {
+  const [products, setProducts] = useState([]);
+
+  const { getProductsByCategory } = useContext(ProductContext);
+
+  useEffect(() => {
+    getProductsByCategory(categoriaTitle).then((res) =>
+      setProducts(res)
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <section className='categoriaContainer'>
-        <div className="title-link">
-            <h2>{categoriaTitle}</h2>
-            <a href={'/productos/' + categoriaTitle}>Ver {categoriaTitle}</a>
-        </div>
-        <Carrusel items={CATEGORY_PRODUCTS} variant='categoria' />
+    <section className="categoriaContainer">
+      <div className="title-link">
+        <h2>{categoriaTitle}</h2>
+        <a href={"/productos/" + categoriaTitle}>Ver {categoriaTitle}</a>
+      </div>
+      {products ? (
+        <Carrusel items={products} variant="categoria" />
+      ) : (
+        <Loader />
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default CategoriaSection
+export default CategoriaSection;

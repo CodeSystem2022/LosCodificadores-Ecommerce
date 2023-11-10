b3bd38268f07302890f2fd7b766e04554c23f1ec
Agustin Vera <ronedev25@gmail.com>
2023-11-10 03:56:11 -0300

    feat: update marcasSection

---
+++ client/src/components/home/marcasSection/Marcas.js
@@ -0,0 +1,82 @@
+import logoAcer from '../../../assets/marcas/acer/logo.jpg'
+import ilustrativeAcer from '../../../assets/marcas/acer/ilustrative.jpg'
+import logoAmd from '../../../assets/marcas/amd/logo.jpg'
+import ilustrativeAmd from '../../../assets/marcas/amd/ilustrative.jpg'
+import logoApple from '../../../assets/marcas/apple/logo.jpg'
+import ilustrativeApple from '../../../assets/marcas/apple/ilustrative.jpg'
+import logoHp from '../../../assets/marcas/hp/logo.jpg'
+import ilustrativeHp from '../../../assets/marcas/hp/ilustrative.jpg'
+import logoIntel from '../../../assets/marcas/intel/logo.jpg'
+import ilustrativeIntel from '../../../assets/marcas/intel/ilustrative.jpg'
+// import logoKingstone from '../../../assets/marcas/kingstone/logo.jpg'
+// import ilustrativeKingstone from '../../../assets/marcas/kingstone/ilustrative.jpg'
+import logoLenovo from '../../../assets/marcas/lenovo/logo.jpg'
+import ilustrativeLenovo from '../../../assets/marcas/lenovo/ilustrative.jpg'
+import logoLg from '../../../assets/marcas/lg/logo.jpg'
+import ilustrativeLg from '../../../assets/marcas/lg/ilustrative.jpg'
+import logoLogitech from '../../../assets/marcas/logitech/logo.jpg'
+import ilustrativeLogitech from '../../../assets/marcas/logitech/ilustrative.jpg'
+import logoReddragon from '../../../assets/marcas/reddragon/logo.jpg'
+import ilustrativeReddragon from '../../../assets/marcas/reddragon/ilustrative.jpg'
+import logoSamsung from '../../../assets/marcas/samsung/logo.jpg'
+import ilustrativeSamsung from '../../../assets/marcas/samsung/ilustrative.jpg'
+import logoSentey from '../../../assets/marcas/sentey/logo.jpg'
+import ilustrativeSentey from '../../../assets/marcas/sentey/ilustrative.jpg'
+
+export const MARCAS = [
+    {
+      id: 0,
+      logo: logoAcer,
+      image: ilustrativeAcer,
+    },
+    {
+      id: 1,
+      logo: logoAmd,
+      image: ilustrativeAmd,
+    },
+    {
+      id: 2,
+      logo: logoApple,
+      image: ilustrativeApple,
+    },
+    {
+      id: 3,
+      logo: logoHp,
+      image: ilustrativeHp,
+    },
+    {
+      id: 4,
+      logo: logoIntel,
+      image: ilustrativeIntel,
+    },
+    {
+      id: 5,
+      logo: logoLenovo,
+      image: ilustrativeLenovo,
+    },
+    {
+      id: 6,
+      logo: logoLg,
+      image: ilustrativeLg,
+    },
+    {
+      id: 7,
+      logo: logoLogitech,
+      image: ilustrativeLogitech,
+    },
+    {
+      id: 8,
+      logo: logoReddragon,
+      image: ilustrativeReddragon,
+    },
+    {
+      id: 9,
+      logo: logoSamsung,
+      image: ilustrativeSamsung,
+    },
+    {
+      id: 10,
+      logo: logoSentey,
+      image: ilustrativeSentey,
+    },
+  ];
--- client/src/components/home/marcasSection/MarcasSection.jsx
+++ client/src/components/home/marcasSection/MarcasSection.jsx
@@ -1,50 +1,6 @@
 import "./MarcasSection.css";
-import asusLogo from "../../../assets/marcas/asusLogo.png";
-import asusProducts from "../../../assets/marcas/asusProducts.jpg";
 import Carrusel from "../../commons/Carrusel/Carrusel";
-
-const MARCAS = [
-  {
-    id: 0,
-    logo: asusLogo,
-    image: asusProducts,
-  },
-  {
-    id: 1,
-    logo: asusLogo,
-    image: asusProducts,
-  },
-  {
-    id: 2,
-    logo: asusLogo,
-    image: asusProducts,
-  },
-  {
-    id: 3,
-    logo: asusLogo,
-    image: asusProducts,
-  },
-  {
-    id: 4,
-    logo: asusLogo,
-    image: asusProducts,
-  },
-  {
-    id: 5,
-    logo: asusLogo,
-    image: asusProducts,
-  },
-  {
-    id: 6,
-    logo: asusLogo,
-    image: asusProducts,
-  },
-  {
-    id: 7,
-    logo: asusLogo,
-    image: asusProducts,
-  },
-];
+import { MARCAS } from "./Marcas";
 
 const MarcasSection = () => {
   return (
@@ -53,7 +9,7 @@ const MarcasSection = () => {
         <h2>Garantizamos eficiencia y calidad</h2>
         <span>Algo lindo que se nos ocurra</span>
       </div>
-      <Carrusel items={MARCAS} />
+      <Carrusel items={MARCAS} itemsPerView={5}/>
     </section>
   );
 };
